496a8e00ffeb2ed6a7dacc2b6d396fa3d6673c74
BorisMunro <munroboris@gmail.com>
2023-12-16 00:53:08 +0800

    feat: updated countrySelect

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -1,9 +1,69 @@
 'use client';
 
-const CountrySelect = () => {
+import Select from 'react-select';
+
+import useCounties from '@/app/hooks/useCountries';
+
+export type CountrySelectValue = {
+    flag: string,
+    label: string,
+    lating: number[],
+    region: string,
+    value: string
+}
+
+interface CountrySelectProps {
+    value?: CountrySelectValue,
+    onChange: (value: CountrySelectValue) => void 
+}
+
+const CountrySelect: React.FC<CountrySelectProps> = ({
+    value,
+    onChange
+}) => {
+    const { getAll } = useCounties();
+
     return ( 
         <div>
-            
+            <Select 
+                placeholder="Anywhere"
+                isClearable
+                options={getAll()}
+                value={value}
+                onChange={(value) => onChange(value as CountrySelectValue)}
+                formatOptionLabel={(option: any) => (
+                    <div
+                        className='
+                            flex
+                            flex-row
+                            tiems-center
+                            gap-3
+                        '  
+                    >
+                        <div>
+                            {option.flag}
+                        </div>
+                        <span className='text-neutral-800 ml-1'>
+                            {option.region}
+                        </span>
+                    </div>
+                )}
+                classNames={{
+                    control: () => 'p-3 border-2',
+                    input: () => 'text-lg',
+                    option: () => 'text-lg'
+                }}
+                theme={(theme) => ({
+                    ...theme,
+                    borderRadius: 6,
+                    colors: {
+                        ...theme.colors,
+                        primary: 'black',
+                        primary25: '#ffe4e6'
+
+                    }
+                })}
+            />
         </div>
      );
 }
